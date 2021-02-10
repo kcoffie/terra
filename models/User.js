960ef433491a0e64edd89ad6231fd4536638dc04
Kate Coffie <kcoffie@directcommerce.com>
2021-02-09 16:45:35 -0800

    .

--- models/User.js
+++ models/User.js
@@ -5,31 +5,32 @@ var jwt = require('jsonwebtoken');
 var secret = require('../config').secret;
 
 var UserSchema = new mongoose.Schema({
-    username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]$/, 'is invalid'], index: true},
-    email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S@\S+\.\S+/, 'is invalid'], index: true},
+    username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid22'], index: true },
+    email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S@\S+\.\S+/, 'is invalid33'], index: true },
     bio: String,
-  image: String,
-  hash: String,
-  salt: String
-}, {timestamps: true});
+    image: String,
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
+    hash: String,
+    salt: String
+}, { timestamps: true });
 
-UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
-UserSchema.methods.setPassword = function(password){
+UserSchema.methods.setPassword = function (password) {
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
 
-UserSchema.methods.validPassword = function(password) {
+UserSchema.methods.validPassword = function (password) {
     var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
     return this.hash === hash;
 };
 
-UserSchema.methods.generateJWT = function() {
+UserSchema.methods.generateJWT = function () {
     var today = new Date();
     var exp = new Date(today);
     exp.setDate(today.getDate() + 60);
-    
+
     return jwt.sign({
         id: this._id,
         username: this.username,
@@ -37,7 +38,7 @@ UserSchema.methods.generateJWT = function() {
     }, secret);
 };
 
-UserSchema.methods.toAuthJSON = function(){
+UserSchema.methods.toAuthJSON = function () {
     return {
         username: this.username,
         email: this.email,
@@ -46,4 +47,33 @@ UserSchema.methods.toAuthJSON = function(){
         image: this.image
     };
 };
+UserSchema.methods.toProfileJSONFor = function (user) {
+    return {
+        username: this.username,
+        bio: this.bio,
+        image: this.image || 'https://imagesvc.meredithcorp.io/v3/jumpstartpure/image?url=https://static.onecms.io/wp-content/uploads/sites/37/2016/05/15215821/101490925.jpg&w=1280&h=720&q=90&c=cc',
+        following: false  // we'll implement following functionality in a few chapters :)
+    };
+};
+
+UserSchema.methods.favorite = function (id) {
+    console.log('fave '+id)
+    console.log('this.favorites.indexOf(id) '+this.favorites.indexOf(id))
+    if (this.favorites.indexOf(id) === -1) {
+        this.favorites = this.favorites.concat(id);
+    }
+    return this.save();
+};
+
+UserSchema.methods.unfavorite = function(id){
+    this.favorites.remove( id );
+    return this.save();
+};
+
+UserSchema.methods.isFavorite = function(id){
+    return this.favorites.some(function(favoriteId){
+      return favoriteId.toString() === id.toString();
+    });
+};
+
 mongoose.model('User', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,40 +4,6 @@ var passport = require('passport');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
-router.post('/users', function (req, res, next) {
-    var user = new User();
-    console.log("uname"+req.body.user.username);
-
-    user.username = req.body.user.username;
-    user.email = req.body.user.email;
-    user.setPassword(req.body.user.password);
-
-    user.save().then(function () {
-        return res.json({ user: user.toAuthJSON() });
-    }).catch(next);
-});
-
-router.post('/users/login', function (req, res, next) {
-    if (!req.body.user.email) {
-        return res.status(422).json({ errors: { email: "can't be blank" } });
-    }
-
-    if (!req.body.user.password) {
-        return res.status(422).json({ errors: { password: "can't be blank" } });
-    }
-
-    passport.authenticate('local', { session: false }, function (err, user, info) {
-        if (err) { return next(err); }
-
-        if (user) {
-            user.token = user.generateJWT();
-            return res.json({ user: user.toAuthJSON() });
-        } else {
-            return res.status(422).json(info);
-        }
-    })(req, res, next);
-});
-
 router.get('/user', auth.required, function (req, res, next) {
     User.findById(req.payload.id).then(function (user) {
         if (!user) { return res.sendStatus(401); }
@@ -73,5 +39,43 @@ router.put('/user', auth.required, function (req, res, next) {
     }).catch(next);
 });
 
+router.post('/users/login', function (req, res, next) {
+    if (!req.body.user.email) {
+        return res.status(422).json({ errors: { email: "can't be blank" } });
+    }
+
+    if (!req.body.user.password) {
+        return res.status(422).json({ errors: { password: "can't be blank" } });
+    }
+
+    passport.authenticate('local', { session: false }, function (err, user, info) {
+        if (err) { return next(err); }
+
+        if (user) {
+            user.token = user.generateJWT();
+            return res.json({ user: user.toAuthJSON() });
+        } else {
+            return res.status(422).json(info);
+        }
+    })(req, res, next);
+});
+
+router.post('/users', function (req, res, next) {
+    var user = new User();
+    console.log("uname "+req.body.user.username);
+    console.log("email "+req.body.user.email);
+    console.log("passwd "+req.body.user.password);
+
+    user.username = req.body.user.username;
+    user.email = req.body.user.email;
+    user.setPassword(req.body.user.password);
+
+    console.log("passwd "+req.body.user.password);
+
+    user.save().then(function () {
+        console.log("passwd "+req.body.user.password);
+        return res.json({ user: user.toAuthJSON() });
+    }).catch(next);
+});
 
 module.exports = router;
